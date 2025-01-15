5225b6d2f11e4658e679a3cf9ecf9e3354f778de
kjh970519 <115525671+kjh970519@users.noreply.github.com>
2025-01-15 16:12:42 +0900

    init

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'http://www.web-bro.ct.ws/php/index.php?type=getStocksNews',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'Cookie: __test=34e0a0997d7ee3a4c2768d203b4147f5',
+            'Host: www.web-bro.ct.ws',
+            'user-agent: Mozilla/4.0 (compatible; cron-job.org; http://cron-job.org/abuse/)'
+        ),
+    ));
+    
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    echo $response;
+
+?>
